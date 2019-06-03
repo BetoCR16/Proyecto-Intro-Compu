9993dc9df4e36fe20c34187a89480d739d85e536
Roberto Méndez <rcarlosml16@gmail.com>
2019-06-02 20:45:50 -0600

    Condiciones en elección

--- 15 en fichas.py
+++ 15 en fichas.py
@@ -1,7 +1,9 @@
 import random
+
 def revisarEleccion():
     eleccion = input('Elija un número de 1 a 9: ')
-    while len(eleccion) != 1 or eleccion not in '123456789':
+    while len(eleccion) != 1 or eleccion not in '123456789' or eleccion in f1 or eleccion in f2 \
+        or eleccion in f3 or eleccion in pc1 or eleccion in pc2 or eleccion in pc3:
         eleccion = input('Elija un número de 1 a 9: ')
     return eleccion
 
@@ -9,23 +11,29 @@ def preguntaInicio():
     iniciar = input('¿Quién desea iniciar?\n1.Usuario\n2.Computadora\nSu elección: ')
     return iniciar
 
-def tablero(turno, numDisponible, numCompu, numUsuario):
-    print (turno)
+def ganar(f1,f2,f3,pc1,pc2,pc3):
+    f1 = int(f1)
+    f2 = int(f2)
+    f3 = int(f3)
+    pc1 = int(pc1)
+    pc2 = int(pc2)
+    pc3 = int(pc3)
+    sumaUsuario = f1 + f2 + f3
+    sumaCompu = pc1 + pc2 + pc3
+    if sumaCompu == 15:
+        print ('Gana compu') 
+    elif sumaUsuario == 15:
+        print('Gana usuario')
     
 
-
-
-
+#----------------------------------------- PRINCIPAL -------------------------------------------------------------------------------
 
 opciones = ['1','2','3','4','5','6','7','8','9']
+pc1 = '7'
+pc2 = '6'
+pc3 = '2'
+f1 = '9'
+f2 = '6'
+f3 = '1'
 
-inicio = preguntaInicio()
-if inicio == '1':
-    f1 = revisarEleccion()
-elif inicio == '2':
-    pc1 = random.choice(opciones)
-
-pc2 = random.choice(opciones)
-
-f2 = revisarEleccion()
-f3 = revisarEleccion()
+ganar(f1,f2,f3,pc1,pc2,pc3)
