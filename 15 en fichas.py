eaf511b2df2238af645383488b1bd502ee4dde2b
Roberto Méndez <rcarlosml16@gmail.com>
2019-06-09 21:36:45 -0600

    Update 15 en fichas.py

--- 15 en fichas.py
+++ 15 en fichas.py
@@ -1,123 +1,132 @@
 import random
+import time
 
-def revisarEleccion():
-    eleccion = int(input('Elija un número de 1 a 9: '))
+def revisarEleccion(): #Evita que el usuario ponga otra cosa que no sea un número del 1 al 9 y evita que se repitan fichas
+    eleccion = int(input('\nSeleccione una ficha: '))
     while len(str(eleccion)) != 1 or str(eleccion) not in '123456789' or eleccion in eleccionesUsuario or eleccion in eleccionesPC:
-        eleccion = int(input('Elija un número de 1 a 9: '))
+        eleccion = int(input('Seleccione una ficha: '))
     return eleccion
 
-def revisarEleccionPC():
+def revisarEleccionPC(): #Evita que la compu repita una ficha que ya haya usado o que el usuario haya usado
     pc = random.choice(opciones)
     while pc in eleccionesUsuario or pc in eleccionesPC:
         pc = random.choice(opciones)
     return pc
 
-def Inicio():
-    print ('Bienvenido al juego de 15 en fichas :D')
-    iniciar = input('¿Quién desea iniciar?\n1.Usuario\n2.Computadora\nSu elección: ')
-    if iniciar == '1':
-        iniciaUsuario()
-    elif iniciar == '2':
-        iniciaPC()
-
-def ganar():
-    sumaUsuario = sumaU(eleccionesUsuario)
-    sumaCompu = sumaPC(eleccionesPC)
-    if sumaCompu == 15:
-        print ('Gana compu')
-        return True
-    elif sumaUsuario == 15:
-        print('Gana usuario')
-        return True
-    else:
-        print('Nadie gana')
+def iniciaUsuario(): #Primer turno si va a iniciar primero el usuario
+    tablero(fichasJugador, fichasCompu)
+    f1 = revisarEleccion()
+    eleccionesUsuario.append(f1)
+    tablero(fichasJugador, fichasCompu)
     
+    pc1 = revisarEleccionPC()
+    eleccionesPC.append(pc1)
+    print('\nFicha #1 compu = ', pc1)
+    time.sleep(2)
+    tablero(fichasJugador, fichasCompu)
+    
+    f2 = revisarEleccion()
+    eleccionesUsuario.append(f2)
+    tablero(fichasJugador, fichasCompu)
+    
+    pc2 = revisarEleccionPC()
+    eleccionesPC.append(pc2)
+    print('\nFicha #2 compu = ', pc2)
+    time.sleep(2)
+    tablero(fichasJugador, fichasCompu)
+    
+    f3 = revisarEleccion()
+    eleccionesUsuario.append(f3)
+    tablero(fichasJugador, fichasCompu)
+    
+    pc3 = revisarEleccionPC()
+    eleccionesPC.append(pc3)
+    print('\nFicha #3 compu = ', pc3)
+    time.sleep(2)
+    tablero(fichasJugador, fichasCompu)
+    
+def iniciaPC(): #Primer turno si va a iniciar primero la computadora
+    tablero(fichasJugador, fichasCompu)
+    
+    pc1 = revisarEleccionPC()
+    eleccionesPC.append(pc1)
+    print('\nFicha #1 compu = ', pc1)
+    time.sleep(2)
+    tablero(fichasJugador, fichasCompu)
 
-def sumaPC(eleccionesPC):
+    f1 = revisarEleccion()
+    eleccionesUsuario.append(f1)
+    tablero(fichasJugador, fichasCompu)
+
+    pc2 = revisarEleccionPC()
+    eleccionesPC.append(pc2)
+    print('\nFicha #2 compu = ', pc2)
+    time.sleep(2)
+    tablero(fichasJugador, fichasCompu)
+
+    f2 = revisarEleccion()
+    eleccionesUsuario.append(f2)
+    tablero(fichasJugador, fichasCompu)
+    
+    pc3 = revisarEleccionPC()
+    eleccionesPC.append(pc3)
+    print('\nFicha #3 compu = ', pc3)
+    time.sleep(2)
+    tablero(fichasJugador, fichasCompu)
+
+    f3 = revisarEleccion()
+    eleccionesUsuario.append(f3)
+    tablero(fichasJugador, fichasCompu)
+    
+def sumaPC(eleccionesPC): #Realiza la suma de las fichas de la compu
     sumaPC = 0
     for i in eleccionesPC:
         sumaPC += i
     return sumaPC   
 
-def sumaU(eleccionesUsuario):
+def sumaU(eleccionesUsuario): #Suma las fichas del usuario
     sumaU = 0
     for i in eleccionesUsuario:
         sumaU += i
     return sumaU
 
-def tablero(turno, fichasJugador, fichasCompu):
+def tablero(fichasJugador, fichasCompu): #Tablero que se va a mostrar en el juego
     print('\n'*30)
-    print('Turno: ', turno)
+    print ('Fichas: ', opciones)
     print('Fichas de usuario: ' ,eleccionesUsuario)
     print('Fichas de computadora: ' , eleccionesPC)
 
-def cambioFicha():
-    fichaCambia = int(input('¿Cuál ficha desea cambiar?\nFicha #: '))
+'''def cambioFicha(): #Pide la ficha a cambiar y la cambia por otra
+    fichaCambia = int(input('\n¿Cuál ficha desea cambiar?\nFicha #: '))
     eleccionesUsuario[fichaCambia - 1] = int(input('Nueva posición de ficha: '))
 
-def cambiandoFichas(turno):
-    while not ganar():
-        turno += 1
-        tablero(turno,fichasJugador, fichasCompu)
-        ganar()
+def cambiandoFichas(): #Va pidiendo cambio de ficha hasta que alguien gane
+    while ganar:
         cambioFicha()
+        tablero(fichasJugador, fichasCompu)
+        ganar()'''
 
-def iniciaUsuario():
-    tablero(turno, fichasJugador, fichasCompu)
-    f1 = revisarEleccion()
-    eleccionesUsuario.append(f1)
-
-    pc1 = revisarEleccionPC()
-    eleccionesPC.append(pc1)
-    print('\nElección de ficha 1 compu = ', pc1)
-
-    f2 = revisarEleccion()
-    eleccionesUsuario.append(f2)
-
-    pc2 = revisarEleccionPC()
-    eleccionesPC.append(pc2)
-    print('\nElección de ficha 2 compu = ', pc2)
-
-    f3 = revisarEleccion()
-    eleccionesUsuario.append(f3)
-
-    pc3 = revisarEleccionPC()
-    eleccionesPC.append(pc3)
-    print('\nElección de ficha 3 compu = ', pc3)
-
-    tablero(turno, fichasJugador, fichasCompu)
-
-def iniciaPC():
-    tablero(turno, fichasJugador, fichasCompu)
+def ganar(): #Define si alguien gana o no
+    sumaUsuario = sumaU(eleccionesUsuario)
+    sumaCompu = sumaPC(eleccionesPC)
+    if sumaCompu == 15:
+        print ('\nGana compu')
+        return True
+    elif sumaUsuario == 15:
+        print('\nGana usuario')
+        return True
+    else:
+        print('\nNadie gana')
+        return False
     
-    pc1 = revisarEleccionPC()
-    eleccionesPC.append(pc1)
-    print('\nElección de ficha 1 compu = ', pc1)
-
-    f1 = revisarEleccion()
-    eleccionesUsuario.append(f1)
-
-    pc2 = revisarEleccionPC()
-    eleccionesPC.append(pc2)
-    print('\nElección de ficha 2 compu = ', pc2)
-
-    f2 = revisarEleccion()
-    eleccionesUsuario.append(f2)
-    
-    pc3 = revisarEleccionPC()
-    eleccionesPC.append(pc3)
-    print('\nElección de ficha 3 compu = ', pc3)
-
-    f3 = revisarEleccion()
-    eleccionesUsuario.append(f3)
-
-    tablero(turno, fichasJugador, fichasCompu)
 #----------------------------------------- PRINCIPAL -------------------------------------------------------------------------------
 
 opciones = [1,2,3,4,5,6,7,8,9]
+continuar = True
+
+
 eleccionesUsuario = []
 eleccionesPC = []
-
 pc1 = 0
 pc2 = 0
 pc3 = 0
@@ -128,5 +137,20 @@ turno = 0
 fichasCompu = ''
 fichasJugador = ''
 
-Inicio()
-ganar()
+print ('Bienvenido al juego de 15 en fichas :D')
+pantallaDeInicio = input('\n1. JUGAR\n2. COMO JUGAR\n3. SALIR\n\nOPCIÓN: ')
+while pantallaDeInicio != '3':
+    if pantallaDeInicio == '1':
+        iniciar = input('¿Quién desea iniciar?\n1.Usuario\n2.Computadora\nSu elección: ')
+        if iniciar == '1':
+            iniciaUsuario()
+            ganar()
+        elif iniciar == '2':
+            iniciaPC()
+            ganar()
+    elif pantallaDeInicio == '2': 
+        print("\nEl juego es simple, primero seleccionas 3 fichas, para ganar, esas fichas \nsumadas deben dar 15. Jugaras contra el computador. \nEl primero que consiga llegar a 15 gana.")
+        pantallaDeInicio = input('\n1. JUGAR\n2. COMO JUGAR\n3. SALIR\n\nOPCIÓN: ')
+print('Muchas gracias, vuelva pronto')
+
+    
